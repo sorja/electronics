9f7f5bf1ae54cd59de3248a7afbd9e1145ee30ee
sorja <sorja@users.noreply.github.com>
2017-05-10 14:14:45 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,171 @@
+
+const ol = document.createElement("ol")
+ol.id = "list"
+document.getElementById("container").appendChild(ol)
+// thanks chrome for xorigin
+getItems = () => {
+    return `5Pcs L7805 Lm7805 7805 +5V 1.5A Voltage Regulator Ic New F
+                2Pcs Tl074 Tl074cn Op Amp Quad Jfet Dip-14 Ic New H
+                5PCS IC TL072CP TL072 DIP-8 chorus delay OP AMPS NEW 
+                10Pcs Lm336-2.5V Lm336 Voltage Reference Diode 2.5V To-92 Voltage Regulator Ne Z
+                100PCS 2N3906 TO-92 General Propose PNP Transistor
+                1/2/5/10PCS ESP8266 ESP-12E Wireless Remote Serial WIFI Transceiver Board Module
+                100Pcs 5mm 4pin RGB Tri-Color Common Cathode LED light Red Green Blue CF
+                10Pcs Prototype Paper PCB Universal Experiment Matrix Circuit Board Stripboard
+                5Pcs LM317T LM317 Voltage Regulator IC 1.2V to 37V 1.5 GM
+                100Pcs 1N4148 In4148 Switching Signal Doide Do-35 Ic New M
+                2PCS 50A 1000V Metal Case Single Phases Diode Bridge Rectifier KBPC5010 
+                2PCS Mini SD Card Module Memory Module Micro SD TF Card Module Arduino ARM AVR
+                66Pcs DIP IC Sockets Adaptor Solder Type Socket Kit 6/8/14/16/18/20/24/28 Pins
+                1 mm 18 AWG Gauge 130 gr ~18 m (4.5 oz) Enameled Copper Magnet Wire Coil
+                1.5M/5ft 2.0mm Copper Desoldering Braid Solder Remover Wick Wire Cable CA
+                FTDI FT232RL USB zu TTL Serien Converter Adapter Modul 5V 3.3V Für Arduino
+                5pcs 15x17mm Black Knob Cap Mini Aluminum Alloy Ø6mm Potentiometer Knobs
+                Hot Non-Contact LCD IR Laser Infrared Digital Temperature Thermometer Gun AR
+                5PCS Soil Hygrometer Detection Module Soil Moisture Sensor Probes New
+                RF transmitter and receiver link kit for Arduino/ARM/MC U remote control O9
+                Wire Stripper Crimper Automatic 3 in 1 Electrical Terminal Cable Cutter Crimp
+                10PCS IRLB8743 IC Chip TO-220 TOP Quality
+                150mm 6" LCD Vernier Caliper Digital Electronic Carbon Fiber Micrometer Gauge
+                15 values 3296 trimmer trim pot Resistor potentiometer kits 15pcs (one each ) 
+                25/10/20pcs CCTV DC Power Female Male Connector Cable Pigtail Plug Wire 5.5x2.1mm
+                20pcs Mini Slide Switch SPDT 2.0mm Pitch 2 Tap Position 3Pin 4.44*8.72*3.95mm B
+                10pcs 8*8mm 6pin DIP Self-Lock Type Square Push Button Contact Switch
+                10PCS SN74HC595N 74HC595D 8-Bit Serial Input/Output Parallel Register DIP-16
+                2PCS MAX7221CNG MAX7221 MAXIM DRVR DSPLY LED 8DIG 24-DIP NEW GOOD QUALITY
+                10Pcs ULN2803A ULN2803 2803 TRANSISTOR ARRAY-8 NPN IC
+                2pcs NEW CD4021 CD4021BE CMOS STATIC SHIFT REG DIP16 DIP-16 TI IC 
+                1PCS Slave HC-06 Wireless Bluetooth Transeiver RF Master Module for Arduino
+                5pcs LM3915N-1 DIP-18 LM3915 DIP18 NSC Dot/Bar Dispaly Driver 
+                10 pcs 100K ohm Linear Taper Rotary Potentiometer Panel pot B100K 15mm NEW 
+                5 10 20 50 pcs 1W 3W High Power 8Type UV ultraviolet 365-410nm LED Lamp Light
+                New 300pcs 30value 1ohm-3M 1/2W Carbon Film Resistor Assortment Kits Set
+                100Pcs 10 Values 102K-681K 2KV High-voltage Ceramic Capacitors Assorted Kit  
+                10PCS DS1307 DS1307N DIP-8 RTC SERIAL 512K I2C Real-Time Clock  IC
+                5Pcs IR Receiver Module 38 KHz TSOP4838 DIP-3
+                Mini Direct Current 0.36 Inch LED Digital Display Tube Ammeter Meter Panel New
+                64 values 1280pcs 1 ohm - 10M ohm 1/4W Metal Film Resistors  Assortment Kit 
+                1PCS 3-wire 0.36"LED DC Digital Voltmeter Panel Meter 0-32V Blue COLOR
+                Solderless Breadboard Protoboard Tie-point 3220 Hole PCB Prototype Board
+                Keypad Shield LCD1602 Starter Kit for Arduino Duemilanove UNO MEGA2560 MEGA1280
+                1000 Pcs 50 Values Ceramic Disc Capacitors Kits New
+                DC-DC Adjustable Step-down Power Supply Module Voltage Current LCD Display Shell
+                5PCS 3 Pin 4 Pin 5 pin JST Connector Male &amp; Female Plug Socket Connecting Cable
+                P6100 100MHz Oscilloscope Scope Clip Probe 100MHz For Tektronix HP DY
+                Mega 2560 R3 REV3 ATmega2560-16AU Board USB Cable Compatible For Arduino#DB
+                DC-DC Adjustable Buck Step-Down Power Supply Module 40V 5A 75W Display Voltmeter
+                1 Pair Banana Plug To Test Hook Clip Probe Lead Cable For Multimeter new
+                10PCS TO-220 Silver Heatsink Heat Sink for Voltage Regulator or MOSFET
+                10 pcs 50K ohm Linear Taper Rotary Potentiometer Panel pot B50K 15mm
+                3-5V 0.96" SPI Serial 128X64 OLED LCD LED Display Module blue yellow  Arduino D
+                5 Pcs 100x60x25mm DIY Plastic Electronic Project Box Enclosure Instrument Case 
+                1M 2~6 Multicore Electronic Wire Cable Points/Signal Small Power Data AUTO CAR
+                127Pcs Heat Shrink Tube Assortment Wire Wrap Electrical Cable Insulation Tubing
+                Stabilizer DC Power Supply 30V 10A 220V|Precision Adjustable LED Digital|EU SWTG
+                45 * 45 * 19mm Aluminum PCB Instrument Box Enclosure Electronic Project Case DIY
+                100Pcs Mixed Color 3mm LED Light Bulb Emitting Diode Bulbs Lamps Colorful Lights
+                Luxury PU Leather Magnetic Flip Stand Card Slot  Wallet Case Cover For LG
+                Solder Sucker Desoldering Pump Tool Removal Vacuum Soldering Iron Desolver  New
+                ESP8266 Serial WIFI Wireless Transceiver Module Send Receive LWIP AP+STA A
+                10PCS CD4050 CD4050BE IC BUFF/CONVERTER HEX DIP16 NEW
+                10x IRF9540 P-Channel Power MOSFET 23A 100V TO-220 "IR" 
+                Welding Soldering Solder Iron Tip Cleaner Cleaning Steel Wire With Stand Set FE
+                10PCS 5 X 7 cm Prototype Paper PCB Universal Experiment Matrix Circuit Board  
+                Luxury Thin Mirror Back Cover + Metal Aluminum Frame Case For Sony Xperia Phones
+                5PCS ATMEL DIP-8 ATTINY85-20PU Tiny85-20PU CHIP IC
+                50PCS MOSFET Transistor CHANGJIANG TO-92 2N7000 New
+                5PCS USB Nano V3.0 ATmega328 16M 5V Micro-controller CH340G board For Arduino
+                New Pro Mini atmega328 5V 16M Replace ATmega128 Arduino Compatible Nano
+                Vintage Leather Flip Wallet Card Slot Case Cover For Smart Phones
+                Solderless Breadboard Protoboard 830 Tie Points 2 buses Test Circuit good MB-102
+                100g 1mm 60/40 Flux Reel/Tube Tin Lead Rosin Core Soldering Wire Welding Iron   
+                20 PCS Male &amp; Female 40pin 2.54mm SIL Header Socket Row Strip PCB Connector Cool
+                5X Clamp Third Hand Soldering Solder Iron Stand Holder Helping Magnifier Tool
+                AC 100-240V to DC 5V 6V 7.5V 9V 1A 2A Switching Power Supply Adapter Charger
+                30m 24AWG Silicone Wire 5 color Mix box 1 package Electrical Wire Line Copper
+                2pcs famale 5.5 x 2.1mm DC Power 12V~36V Jack Adapter Connector Plug for CCTV
+                15PCS Values Crystal Oscillator Assortment 4-48MHZ Kit Set DIP DIY
+                10PCS IRF3205 3205 N-CHANNEL 110A 55V MOSFET
+                Ultimate 37 in 1 Sensor Modules Kit for Arduino MCU Education User + Free case S
+                300pcs Component Resistance Pack 1% 1/4W Metal Film Resistor 10 ~ 1M ohm Useful
+                LCD Auto Range A/DC Digital Voltmeter Ammeter Ohmmeter Multimeter Volt Tester CB
+                300pcs (2V – 39V) 30 Values 1/2W 0.5W Zener Diode Assorted Assortment Set Kit 
+                Durable Electrical Wire Cable Cutter Cutting Plier Side Snips Flush Pliers Tool
+                200pcs DIY 15 Values 0.1uF-220uF Electrolytic Capacitor Assortment Kit with Box
+                New Plug Adapter AC100-240V To DC 12V 2A Power Supply For 3528 5050 Strip LED EU
+                NEW UNO R3 ATmega328P CH340 Mini USB Board for Compatible-Arduino
+                120pcs Dupont Wire Male to Male + Male to Female + Female to Female Jumper Cable
+                1W 3W 5W 10W 20W 30W 50W 100W High Power RGB Full Color LED Beads Lamp Chip
+                6Pcs 170 Tie-point Prototype Solderless PCB Breadboard Electronic Experiment QT
+                Plastic Round 8 Grids Pills Jewelry Beads Electronics Storage Box Organiser Case
+                50PCS IC DIP-8 NE555 TI Timers NEW GOOD QUALITY 
+                AC100-240V To DC 12V 1/2/3/5/6/8A Power Supply Adapter Transformer for LED Strip
+                Multifunctional Cable Wire Stripper Stripping Cutter Cutting Plier H Handle Tool
+                5M 10M 15M SMD 3528 5050 5630 300LEDs RGB White LED Strip Light 12V Power Supply
+                100Pcs 8 Values Diode Bag Assortment Kit Set 1N4148 FR107 1N5408 For Arduino
+                MB-102 830 Point Solderless Breadboard PCB +Power Supply+65pcs Jump Cable Wires
+                Resistor 10 Ω
+                Resistor 22 Ω
+                Resistor 47 Ω
+                Resistor 100 Ω
+                Resistor 150 Ω
+                Resistor 200 Ω
+                Resistor 220 Ω
+                Resistor 270 Ω
+                Resistor 330 Ω
+                Resistor 470 Ω
+                Resistor 510 Ω
+                Resistor 680 Ω
+                Resistor 1k Ω
+                Resistor 2k Ω
+                Resistor 2.2k Ω
+                Resistor 3.3k Ω
+                Resistor 4.7k Ω
+                Resistor 5.1k Ω
+                Resistor 6.8k Ω
+                Resistor 10k Ω
+                Resistor 20k Ω
+                Resistor 47k Ω
+                Resistor 51k Ω
+                Resistor 68k Ω
+                Resistor 220k Ω
+                Resistor 300k Ω
+                Resistor 470k Ω
+                Resistor 680k Ω
+                Resistor 1M Ω
+                Capacitor 0.1u
+                Capacitor 0.22u
+                Capacitor 0.47u
+                Capacitor 1u
+                Capacitor 2.2u
+                Capacitor 3.3u
+                Capacitor 4.7u
+                Capacitor 10u
+                Capacitor 22u
+                Capacitor 33u
+                Capacitor 47u
+                Capacitor 47u
+                Capacitor 100u
+                Capacitor 100u
+                Capacitor 220u`.split("\n")
+}
+const renderList = (to, arr, el) => {
+    to.innerHTML = null
+    arr.forEach(
+        (val, index) => {
+            const element = document.createElement(el)
+            element.innerHTML = val
+            to.append(element)
+        })
+}
+renderList(ol, getItems(), "li" )
+
+function filter(val){
+    if (!val) {
+        renderList(ol, getItems(), "li" )
+        return false
+    }
+    t = v => v.toLowerCase().trim()
+    const items = getItems().filter(value => t(value).indexOf(t(val)) >= 0)
+    renderList(ol, items, "li" )
+}
